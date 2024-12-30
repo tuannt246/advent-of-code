8d92b7b499af7b8e8639319434f6fbd5c3f69737
tuannt246 <ttn.thanhtuannguyen@gmail.com>
2024-12-30 16:57:40 +0700

    <2023-3>: gear ratios

--- 2023/day3/index.php
+++ 2023/day3/index.php
@@ -1,30 +1,44 @@
 <?php
 
+const DIRECTIONS = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]
+];
+
 dayThree("input.txt");
 
 function dayThree($fileName)
 {
     $dataStream = readFileContents($fileName);
-    list($first, $second) = findSumOfNumbersAdjacentSymbol($dataStream);
+    $first = findSumOfAdjacentSymbol($dataStream);
+    $second = findSumOfGearRatios($dataStream);
     printResult($first, $second);
 }
 
-function findSumOfNumbersAdjacentSymbol($schematic): array
+function findSumOfAdjacentSymbol($schematic): int
 {
     $sum = 0;
-    $current = '';
-    $isAdjacent = false;
-    $ratio = [];
+    $rowLength = count($schematic);
+    $columnLength = strlen($schematic[0]);
     foreach ($schematic as $row => $line) {
-        if ($isAdjacent && !empty($current)) {
-            $sum += (int)$current;
-        }
         $current = '';
         $isAdjacent = false;
         foreach (str_split($line) as $column => $char) {
             if ((is_numeric($char))) {
                 if (!$isAdjacent) {
-                    $isAdjacent = isAdjacentSymbol($row, $column, $schematic);
+                    foreach (DIRECTIONS as [$dr, $dc]) {
+                        $newRow = $row + $dr;
+                        $newColumn = $column + $dc;
+                        if ($newRow < 0 || $newRow >= $rowLength || $newColumn < 0 || $newColumn >= $columnLength) {
+                            continue;
+                        }
+
+                        if ($schematic[$newRow][$newColumn] != '.' && !is_numeric($schematic[$newRow][$newColumn])) {
+                            $isAdjacent = true;
+                            break;
+                        }
+                    }
                 }
                 $current .= $char;
             } else {
@@ -35,33 +49,66 @@ function findSumOfNumbersAdjacentSymbol($schematic): array
                 $isAdjacent = false;
             }
         }
+        if ($isAdjacent) {
+            $sum += (int)$current;
+        }
     }
 
-    return [$sum, 0];
+    return $sum;
 }
 
-function isAdjacentSymbol($row, $column, $table): bool
+function findSumOfGearRatios($schematic)
 {
-    $directions = [
-        [-1, -1], [-1, 0], [-1, 1],
-        [0, -1], [0, 1],
-        [1, -1], [1, 0], [1, 1]
-    ];
+    $ratios = [];
+    $rowLength = count($schematic);
+    $columnLength = strlen($schematic[0]);
+    foreach ($schematic as $row => $line) {
+        $current = '';
+        $gearPositions = [];
+        $ratio = null;
+        foreach (str_split($line) as $column => $char) {
+            if ((is_numeric($char))) {
+                $current .= $char;
+                foreach (DIRECTIONS as [$dr, $dc]) {
+                    $newRow = $row + $dr;
+                    $newColumn = $column + $dc;
+                    if ($newRow < 0 || $newRow >= $rowLength || $newColumn < 0 || $newColumn >= $columnLength) {
+                        continue;
+                    }
 
-    foreach ($directions as [$dr, $dc]) {
-        $newRow = $row + $dr;
-        $newColumn = $column + $dc;
-        if ($newRow < 0 || $newRow >= count($table) || $newColumn < 0 || $newColumn >= strlen($table[$row])) {
-            continue;
+                    if ($schematic[$newRow][$newColumn] == '*') {
+                        $ratio = "$newRow-$newColumn";
+                        break;
+                    }
+                }
+                if (isset($ratio)) {
+                    $gearPositions[] = $ratio;
+                }
+            } else {
+                if (!empty($current)) {
+                    foreach (array_unique($gearPositions) as $gearPos) {
+                        $ratios[$gearPos][] = intval($current);
+                    }
+                }
+                $gearPositions = [];
+                $current = '';
+                $ratio = null;
+            }
         }
-
-        if ($table[$newRow][$newColumn] != '.' && !is_numeric($table[$newRow][$newColumn])) {
-            return true;
+        if (!empty($current)) {
+            foreach (array_unique($gearPositions) as $gearPos) {
+                $ratios[$gearPos][] = intval($current);
+            }
         }
     }
-    return false;
+    $product = 0;
+    foreach ($ratios as $values) {
+        if (count($values) == 2) {
+            $product += array_product($values);
+        }
+    }
+    return $product;
 }
-
 
 function readFileContents($fileName)
 {
@@ -72,5 +119,7 @@ function printResult($first, $second)
 {
     echo "--- Day 3: Gear Ratios ---" . "\n";
 
-    echo "The sum of all of the calibration values only by numbers is {$first}." . "\n";
+    echo "The sum of all of the part numbers in the engine schematic is {$first}." . "\n";
+
+    echo "The sum of all of the gear ratios in your engine schematic is {$second}." . "\n";
 }
